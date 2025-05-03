eb6db7c4f6b4ac7f6c1472a5caa9fef94d2f55a1
Tarun Jawla <tarunjawla2@gmail.com>
2025-05-03 18:41:54 +0530

    array error when accessing index more than length

--- data-types-in-rust/src/main.rs
+++ data-types-in-rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     println!("Rust Data Types Practice");
 
@@ -33,7 +35,7 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("\n Tuple:");
-    println! ("tup.0 = {}", tup.0);
+    println!("tup.0 = {}", tup.0);
     println!("Destructured tuple is: x ={}, y ={}, z= {}", x, y, z);
 
     //2. Array
@@ -41,4 +43,25 @@ fn main() {
     let arr: [i32; 4] = [10, 20, 30, 40];
     println!("\n Array:");
     println!("arr[0]= {}", arr[0]);
+
+    //what happens when we try to access element of an array outside index
+
+    let new_arr = [1, 4, 6, 7, 8, 9,12];
+
+    println!("Please enter an index.");
+
+    let mut index = String::new();
+    //if we enter an index out of the bound i.e. 90, rust will give the following error: "index out of bounds: the len is 7 but the index is 90"
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = new_arr[index];
+
+    println!("The value of element at index {index} is: {element}:");
 }
